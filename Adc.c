27bb8a68bd252e3cb0091337854f56968433337c
hala shaheen <hala1731998@gmail.com>
2020-07-26 02:48:24 +0200

    avg , interrupt and comparator not supported yet

--- Adc.c
+++ Adc.c
@@ -7,44 +7,83 @@
 
 #include "Adc.h"
 
+// ex: ADC_CLK(ADC_MOD0,CLK_ENABLE);
+void ADC_CLK(ADC_MOD ADC_module,CLK_MODE mode){
+    SYSCTL_RCGCADC_R&=~(1<<ADC_module);
+    SYSCTL_RCGCADC_R|=(mode<<ADC_module);
+}
+
+// ex : uint32_t ADC_ModuleBase=__ADCModuleBase(ADC_MOD0);
+uint32_t __ADCModuleBase(ADC_MOD ADC_module){
+    if(ADC_module==ADC_MOD0) return ADC0_BASE;
+    else if(ADC_module==ADC_MOD1) return ADC1_BASE;
+}
+
+//if the trigger source is one of the pwm generators we have to select the pwm module using this function
+// ex: __ADCTriggerPwmModule(ADC0_BASE,ADC_SS0,PWM_MOD0);
+void __ADCTriggerPwmModule(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,PWM_MOD pwm_module){
+    ADC_REG(ADC_module_base,ADC_O_TSSEL)&=~((0x03)<<(4*(2*ADC_sequencerNumber+1)));
+    ADC_REG(ADC_module_base,ADC_O_TSSEL)|=(pwm_module<<(4*(2*ADC_sequencerNumber+1)));
+}
+
 // ex : __ADCTriggerSet(ADC0_BASE,ADC_SS0,ADC_TRIGGER_PROCESSOR);
-void __ADCTriggerSet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber ,uint16_t ADC_triggerSource){
+void __ADCTriggerSet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber ,uint8_t ADC_triggerSource){
 
-    ADC_REG(ADC_module_base,ADC_O_EMUX)&=~((0x0003)<<ADC_sequencerNumber);
-    ADC_REG(ADC_module_base,ADC_O_EMUX)|=((ADC_triggerSource)<<ADC_sequencerNumber);
-    //for pwm
-    /*
-    if(ADC_triggerSource==ADC_EMUX_EM3_PWM0||ADC_triggerSource==ADC_EMUX_EM3_PWM1||
-       ADC_triggerSource==ADC_EMUX_EM3_PWM2||ADC_triggerSource==ADC_EMUX_EM3_PWM3||
+    ADC_REG(ADC_module_base,ADC_O_EMUX)&=~((0x0F)<<(4*ADC_sequencerNumber));
+    ADC_REG(ADC_module_base,ADC_O_EMUX)|=((ADC_triggerSource)<<(4*ADC_sequencerNumber));
 
-       ADC_triggerSource==ADC_EMUX_EM2_PWM0||ADC_triggerSource==ADC_EMUX_EM2_PWM1||
-       ADC_triggerSource==ADC_EMUX_EM2_PWM2||ADC_triggerSource==ADC_EMUX_EM2_PWM3||
-
-       ADC_triggerSource==ADC_EMUX_EM1_PWM0||ADC_triggerSource==ADC_EMUX_EM1_PWM1||
-       ADC_triggerSource==ADC_EMUX_EM1_PWM2||ADC_triggerSource==ADC_EMUX_EM1_PWM3||
-
-       ADC_triggerSource==ADC_EMUX_EM0_PWM0||ADC_triggerSource==ADC_EMUX_EM0_PWM1||
-       ADC_triggerSource==ADC_EMUX_EM0_PWM2||ADC_triggerSource==ADC_EMUX_EM0_PWM3){}*/
+    if(ADC_triggerSource==ADC_TRIGGER_PWM0||ADC_triggerSource==ADC_TRIGGER_PWM1||
+       ADC_triggerSource==ADC_TRIGGER_PWM2||ADC_triggerSource==ADC_TRIGGER_PWM3){
+        __ADCTriggerPwmModule(ADC_module_base,ADC_sequencerNumber,PWM_MOD0); }
 
 }
 
+
+
 // ex: __ADCPrioritySet(ADC0_BASE,ADC_SS0,ADC_SS_PRI2);
 void __ADCPrioritySet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber ,uint8_t ADC_SequencerPriority){
-    ADC_REG(ADC_module_base,ADC_O_SSPRI)&=~((0x0003)<<(4*ADC_sequencerNumber));
+    ADC_REG(ADC_module_base,ADC_O_SSPRI)&=~((0x03)<<(4*ADC_sequencerNumber));
     ADC_REG(ADC_module_base,ADC_O_SSPRI)|=((ADC_SequencerPriority)<<(4*ADC_sequencerNumber));
 }
 
 
 // ex: ADCSampleSequencerConfigure(ADC0_BASE,ADC_SS0,ADC_TRIGGER_PROCESSOR,ADC_SS_PRI2);
 void ADCSampleSequencerConfigure(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
-                                   uint16_t ADC_triggerSource, uint8_t ADC_SequencerPriority)
+                                 uint8_t ADC_triggerSource,uint8_t ADC_SequencerPriority)
 {
     __ADCTriggerSet(ADC_module_base,ADC_sequencerNumber, ADC_triggerSource);
     __ADCPrioritySet(ADC_module_base,ADC_sequencerNumber, ADC_SequencerPriority);
 }
 
-// ex: ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample1,(AIN0_PE3|X)
-//  X: set or clear each of [ADC_CTL_TS-ADC_CTL_IE-ADC_CTL_END-ADC_CTL_D]
+
+// ex: ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample1,(AIN0_PE3|X))
+//  X: set or clear each of [ADC_CTL_TS-ADC_CTL_IE-ADC_CTL_END-ADC_CTL_D-ADC_CTL_NONE]
+
+/*
+ *if we want (set bit) any of these configurations [ADC_CTL_TS-ADC_CTL_IE-ADC_CTL_END-ADC_CTL_D-ADC_CTL_NONE] we put it ORed in the
+ *second parameter of macro ADC_ConfigPin(pin,config), else if we want to clear bit
+ *we just dont't write the config we don't want.
+ *
+ *1-ADC_CTL_TS  ->  0: the input pin analog value is read and converted
+ *                  1: the temperature sensor is read and converted
+ *
+ *2-ADC_CTL_IE  ->  related to interrupt
+ *3-ADC_CTL_END ->  0: sampling and conversion continues
+ *                  1: the end of the sequence
+ *
+ *4-ADC_CTL_D   ->  0: single-ended
+ *                  1: differential and both pair-pins must be configured
+ *
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample0,ADC_ConfigPin(AIN8_PE5,ADC_CTL_TS|ADC_CTL_IE));
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample1,ADC_ConfigPin(AIN10_PB4,ADC_CTL_TS));
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample2,ADC_ConfigPin(AIN0_PE3,ADC_CTL_END));
+ *
+ * if we choose differential end we have to configure both pins for example
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample0,ADC_ConfigPin(AIN0_PE3,ADC_CTL_D));
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample1,ADC_ConfigPin(AIN1_PE2,ADC_CTL_D));
+ * ADCSampleSequencerPinConfigure(ADC0_BASE,ADC_SS0,ADC_Sample2,ADC_ConfigPin(AIN9_PE4,ADC_CTL_END));
+ *
+ */
 void ADCSampleSequencerPinConfigure(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
                                     ADC_SampleOrder ADC_PinSampleOrder ,uint8_t ADC_PinConfig)
 {
@@ -67,13 +106,16 @@ void ADCSampleSequencerPinConfigure(uint32_t ADC_module_base, uint8_t ADC_sequen
             ADCSSMUXn_offset=ADC_O_SSMUX3;
             ADCSSCTLn_offset=ADC_O_SSCTL3;}
 
+
     //PIN which is the input source at sample of order ADC_PinSampleOrder
-    ADC_REG(ADC_module_base,ADCSSMUXn_offset)&=~((0x000C)<<(4*ADC_PinSampleOrder));
+    ADC_REG(ADC_module_base,ADCSSMUXn_offset)&=~((0x000F)<<(4*ADC_PinSampleOrder));
     ADC_REG(ADC_module_base,ADCSSMUXn_offset)|=((ADC_PIN)<<(4*ADC_PinSampleOrder));
 
     //PIN configurations
-    ADC_REG(ADC_module_base,ADCSSCTLn_offset)&=~((0x00FF)<<(4*ADC_PinSampleOrder));
-    ADC_REG(ADC_module_base,ADCSSCTLn_offset)|=((ADC_PinConfig)<<(4*ADC_PinSampleOrder));
+    ADC_REG(ADC_module_base,ADCSSCTLn_offset)&=~((0x000F)<<(4*ADC_PinSampleOrder));
+    ADC_REG(ADC_module_base,ADCSSCTLn_offset)|=((ADC_PinConfig&0x0F)<<(4*ADC_PinSampleOrder));
+
+    //Disable digital comparator
 
     //==> Interrupt ....
     /*
@@ -82,13 +124,31 @@ void ADCSampleSequencerPinConfigure(uint32_t ADC_module_base, uint8_t ADC_sequen
      */
 }
 
+
+// ex: ADCSampleSequencerEnable(ADC0_BASE,ADC_SS0);
 void ADCSampleSequencerEnable(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber){
     ADC_REG(ADC_module_base,ADC_O_ACTSS)|=(1<<ADC_sequencerNumber);}
 
+// ex: ADCSampleSequencerDisable(ADC0_BASE,ADC_SS0);
 void ADCSampleSequencerDisable(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber){
     ADC_REG(ADC_module_base,ADC_O_ACTSS)&=~(1<<ADC_sequencerNumber);}
 
-int32_t ADCSequenceDataGet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,uint32_t *BufferPtr){
+/*
+ * #define n 8
+ *
+ * uint8_t sample=0;
+ * uint32_t BufferData[n];
+ * uint32_t* BufferPtr=BufferData; //name of array is pointer
+ *
+ * uint32_t n_samples=ADCSequenceDataGet(ADC0_BASE,ADC_SS0,BufferPtr);
+ *
+ * for(;sample<n_sample;sample++)
+ *  BufferData[sample]=*(BufferPtr+sample);
+ *
+ */
+
+//absolutely different approach ...
+uint32_t ADCSequenceDataGet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,uint32_t *BufferPtr){
 
     uint8_t SS_MAXlimit=0;
     uint32_t ADCSSFIFOn_offset,ADCSSFSTATn_offset;
@@ -112,20 +172,70 @@ int32_t ADCSequenceDataGet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber
         ADCSSFSTATn_offset=ADC_O_SSFSTAT3;
         SS_MAXlimit=0x01;}
 
-    //==> pointers need rechecking ...
-     uint32_t * RD_Ptr=ADC_REG(ADC_module_base,ADCSSFSTATn_offset)&read_ptr;
-     uint32_t * WR_Ptr=ADC_REG(ADC_module_base,ADCSSFSTATn_offset)&Write_ptr;
+
+     uint8_t RD_Index=ADC_REG(ADC_module_base,ADCSSFSTATn_offset)&read_ptr;
+     uint8_t WR_Index=(ADC_REG(ADC_module_base,ADCSSFSTATn_offset)&Write_ptr)>>4;
      uint8_t n_samples=0;
-     BufferPtr=RD_Ptr;
-     while(RD_Ptr!=WR_Ptr){
+
+     //pointer=location in FIFO
+     BufferPtr=(uint32_t *)(ADCSSFIFOn_offset+RD_Index);
+
+     while(RD_Index!=WR_Index){
          n_samples++;
-         if(RD_Ptr==SS_MAXlimit){*RD_Ptr=0x00;}
-         RD_Ptr++;
+         if(RD_Index==SS_MAXlimit){RD_Index=0x00;}
+         RD_Index++;
      }
      return n_samples;
 }
+//support both individual ADC sequencer and conncurrent sampling
+/*
+ * individual sampling
+ * --------------------
+ * ADC_InitiateConversionBySW(ADC0_BASE,ADC_SS0,individualSamplingMode,individual);
+ *
+ * concurrent sampling
+ * -------------------
+ *  ADC_InitiateConversionBySW(ADC0_BASE,ADC_SS0,concurrentSamplingMode,Concurrent_notLast);
+ *  ADC_InitiateConversionBySW(ADC1_BASE,ADC_SS2,concurrentSamplingMode,Concurrent_Last);
+ *
+ *  this makes Both ADC module 1 and module 2 start conversion at the same time
+ *
+ *
+ */
+/*
+ *
+ * inside an ADC module
+ *                     - Never all "ss" sample sequencers concurrently sample ,only one at time
+ *                     - the order which one executes before the other is based on the priority of the ss
+ *
+ * ADC module
+ *              - individual mode : means the first configured is the first ready to sample
+ *              - concurrent mode : means both modules will sample at the same time
+ *
+ *
+ */
+void ADC_InitiateConversionBySW(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
+                                ADC_SW_ConversionMode conversionMode,IsLastConcurrentModule flag){
+
+    ADC_REG(ADC_module_base,ADC_O_PSSI)|=(1<<ADC_sequencerNumber); //SSn Initiate
+
+    if(conversionMode==individualSamplingMode)
+        ADC_REG(ADC_module_base,ADC_O_PSSI)&=~(1<<SYNCWAIT_BIT);//Sampling begins when a sample sequence has been initiated.
+    else if(conversionMode==concurrentSamplingMode)
+        ADC_REG(ADC_module_base,ADC_O_PSSI)|=(1<<SYNCWAIT_BIT);//This bit allows the sample sequences to be initiated,
+                                                               //but delays sampling until the GSYNC bit is set.
+    if(flag==Concurrent_Last)
+        ADC_REG(ADC_module_base,ADC_O_PSSI)|=(uint32_t)(1<<GSYNC_BIT);
+
+
+}
+
+
 
 void ADCSequencerProcessorTrigger(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber){
-    //==> not sure ....
-    ADC_REG(ADC_module_base,ADC_O_PSSI)&=~(1<<ADC_sequencerNumber);
+
+    ADC_REG(ADC_module_base,ADC_O_PSSI)|=((0xFFFF0000)|(1<<ADC_sequencerNumber));
 }
+
+
+
--- Adc.h
+++ Adc.h
@@ -2,7 +2,19 @@
  * Adc.h
  *
  *  Created on: Jul 24, 2020
- *      Author: moh1969
+ *      Author: Hala Shaheen
+ */
+
+
+
+/*
+ * we have analog signal so at certain sample we want to know the corresponding digital value ,
+ * so the concept of sample in a point on analg signal converted to digital ,
+ * sequence is group of samples
+ * sample sequencer is responsible for taking N samples where N is dependent on sample sequencer number,
+ * and the end bit [if sample's end of sequence bit is enabled then this is the last sample in sequence
+ * meaning that sequence length is end bit+1 ] samples
+ *
  */
 
 #ifndef ADC_H_
@@ -10,11 +22,18 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "Pwm.h"
+
+#define SYSCTL_RCGCADC_R        (*((volatile uint32_t *)0x400FE638))
 
 typedef enum{
     ADC_MOD0,
     ADC_MOD1
 }ADC_MOD;
+typedef enum{
+    CLK_DIABLE,
+    CLK_ENABLE
+}CLK_MODE;
 
 // ADC PinID
 #define AIN0_PE3 0x00
@@ -59,9 +78,11 @@ typedef enum{
 #define ADC_SS_PRI2 0x02
 #define ADC_SS_PRI3 0x03  /*LOWEST*/
 
+
 // The following are defines for the ADC register offsets
 #define ADC_O_ACTSS             0x00000000  // ADC Active Sample Sequencer
 #define ADC_O_EMUX              0x00000014  // ADC Event Multiplexer Select
+#define ADC_O_TSSEL             0x0000001C  // ADC Trigger Source Select
 #define ADC_O_SSPRI             0x00000020  // ADC Sample Sequencer Priority
 #define ADC_O_PSSI              0x00000028  // ADC Processor Sample Sequence Initiate
 
@@ -104,6 +125,7 @@ typedef enum{
 #define ADC_TRIGGER_PWM_MOD1    0x00000010  // PWM triggers from PWM1
 
 
+#define ADC_ConfigPin(ADC_PIN,CONFIG) ((ADC_PIN<<4)|(CONFIG))
 //                                                7...... 4  3  2  1  0
 // ADC Pin configurations offsets ADC_PinConfig : ADC_PinID TS IE END D
 #define ADC_PinID               0xF0  // PIN_ID
@@ -111,6 +133,8 @@ typedef enum{
 #define ADC_CTL_IE              0x04  // Interrupt enable
 #define ADC_CTL_END             0x02  // Sequence end select
 #define ADC_CTL_D               0x01  // Differential select
+#define ADC_CTL_NONE            0x00  //single ended , not end, disable interrupt, don't ake value of temperature
+                                      // and convert the associated pin analog signal
 
 // ADC FIFO Status Offsets
 #define read_ptr 0x0F
@@ -118,15 +142,31 @@ typedef enum{
 #define EMPTY 0x100
 #define FULL  0x800
 
+// ADC Processor Sample Sequence Initiate (ADC_O_PSSI)
+#define SYNCWAIT_BIT 27
+#define GSYNC_BIT    31
+
+typedef enum {
+    individualSamplingMode,
+    concurrentSamplingMode
+}ADC_SW_ConversionMode;
+
+typedef enum{
+    individual,
+    Concurrent_notLast=0,
+    Concurrent_Last
+}IsLastConcurrentModule;
+
 #define ADC_REG(module_base,reg_offset) (*((volatile uint32_t*) (module_base+reg_offset)))
 
 /*NON-AUTOSAR APIs*/
 
 
-//==> clock functions ....
+
+void ADC_CLK(ADC_MOD ADC_module,CLK_MODE mode);
 
 void ADCSampleSequencerConfigure(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
-                                 uint16_t ADC_triggerSource, uint8_t ADC_SequencerPriority);
+                                 uint8_t ADC_triggerSource, uint8_t ADC_SequencerPriority);
 
 void ADCSampleSequencerPinConfigure(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
                                     ADC_SampleOrder ADC_PinSampleOrder ,uint8_t ADC_PinConfig);
@@ -134,10 +174,36 @@ void ADCSampleSequencerPinConfigure(uint32_t ADC_module_base, uint8_t ADC_sequen
 void ADCSampleSequencerEnable(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber);
 void ADCSampleSequencerDisable(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber);
 
-int32_t ADCSequenceDataGet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
+uint32_t ADCSequenceDataGet(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
                            uint32_t *BufferPtr);
+
+void ADC_InitiateConversionBySW(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber,
+                                ADC_SW_ConversionMode conversionMode,IsLastConcurrentModule flag);
 
 void ADCSequencerProcessorTrigger(uint32_t ADC_module_base, uint8_t ADC_sequencerNumber);
 
 
+/*AUTOSAR APIs*/
+
+/*
+void Adc_Init ( const Adc_ConfigType* ConfigPtr );
+Std_ReturnType Adc_SetupResultBuffer ( Adc_GroupType Group, Adc_ValueGroupType* DataBufferPtr );
+void Adc_DeInit ( void );
+
+
+void Adc_StartGroupConversion ( Adc_GroupType Group );
+void Adc_StopGroupConversion ( Adc_GroupType Group );
+Std_ReturnType Adc_ReadGroup ( Adc_GroupType Group, Adc_ValueGroupType* DataBufferPtr );
+
+void Adc_EnableHardwareTrigger ( Adc_GroupType Group );
+void Adc_DisableHardwareTrigger ( Adc_GroupType Group );
+void Adc_EnableGroupNotification ( Adc_GroupType Group );
+void Adc_DisableGroupNotification ( Adc_GroupType Group );
+Adc_StatusType Adc_GetGroupStatus ( Adc_GroupType Group );
+Adc_StreamNumSampleType Adc_GetStreamLastPointer ( Adc_GroupType Group, Adc_ValueGroupType** PtrToSamplePtr );
+*/
+
 #endif /* ADC_H_ */
+
+
+
